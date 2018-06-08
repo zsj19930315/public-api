e0deaf343c145247327afc111d02dc9ef6cc1484
Administrator <Administrator@admin-4.fjiot.gov.cn>
2018-06-08 11:04:06 +0800

    public-api

---
+++ src/main/java/com/zhengkaix/publicapi/PublicApiApplication.java
@@ -0,0 +1,12 @@
+package com.zhengkaix.publicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicApiApplication.class, args);
+	}
+}
